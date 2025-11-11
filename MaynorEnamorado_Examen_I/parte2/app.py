71327cfe216cd79dc549b2132ce4c45c1ff4efa1
maynor-enamorado02 <mynor.enamorado@unitec.edu>
2025-11-10 21:27:23 -0600

    Parte2

---
+++ MaynorEnamorado_Examen_I/parte2/app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+PORT = int(os.environ.get('PORT', 5000))
+
+@app.route('/')
+def inicio():
+    return jsonify({
+        "mensaje": "Examen Docker",
+        "estudiante": "Maynor Enamorado" 
+    })
+
+@app.route('/salud')
+def salud():
+    return jsonify({"status": "ok"})
+
+if __name__ == '__main__':
+    print(f"Aplicación iniciada en puerto {PORT}")
+    app.run(host='0.0.0.0', port=PORT)
